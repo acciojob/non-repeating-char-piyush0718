426ecd0048406cdcbb5102a246e287753b53f37e
piyush0718 <tpiyush2000@gmail.com>
2025-11-01 19:14:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count = {};
+	for(let ch of str){
+		count[ch] = (count[ch] || 0) + 1;
+	}
+	for(let ch of str){
+		if(count[ch]===1){
+			return ch;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
+let input = prompt("Enter a string"); 
 alert(firstNonRepeatedChar(input)); 
